f442657d1caeb519f81653405c8d1eb9a541a639
moshiurakash <67516496+moshiurakash@users.noreply.github.com>
2020-07-17 20:23:17 +0600

    brickCalculator

--- assignment.js
+++ assignment.js
@@ -23,7 +23,7 @@ function woodCalculator(chair, table, bed)
     {
         return "Physical entity can not be negative"
     }
-    //take inputs and multiply it with cubic feet wood needed for make single one and add all three
+    //take inputs and multiply it with cubic feet wood needed to make single one and add all three
     else
     {
             var chairWood = chair*1;
@@ -34,3 +34,37 @@ function woodCalculator(chair, table, bed)
     return totalWood;
 }
 
+//function for calculating bricks needed to build a multi store building.
+//hight of first ten floor is 15 feet, next 10 floor is 12 feet and after that is 10 feet.
+//bricks neede to build per feet is 1000.
+function brickCalculator(floorCount)
+{
+    //first condition to check if the bulding is more than 20 floors and then calculate needed bricks. for first 20 floor needed bricks are pre-defined.
+    if(floorCount>20)
+    {
+        var excess = floorCount - 20;
+        var feetCount = excess*10;
+        var totalFeet = feetCount + (10*12) + (10*15);
+        var neededBricks = totalFeet*1000;
+    }
+    //second condition to check if the building is more than 10 floors but less than or equal 20 floors. For first 10 floors needed bricks are pre-defined.
+    else if(floorCount>10 && floorCount<=20)
+    {
+        var excess = floorCount - 10;
+        var feetCount = excess*12;
+        var totalFeet = feetCount + (10*15);
+        var neededBricks = totalFeet*1000;
+    }
+    //third condition to check if the bulding is less than or equal 10 floors.
+    else if(floorCount>0 && floorCount<=10)
+    {
+        var feetCount = floorCount*15;
+        var neededBricks = feetCount*1000;
+    }
+    //if input is invalid necessary warnings are given.
+    else
+    {
+        return "Floor can not be negative or you do not need any bricks";
+    }
+    return neededBricks;
+}
